7bdb86c0fd7f14a931ffd07ceb33631be1795ea3
alcen <49450743+alcen@users.noreply.github.com>
2023-02-15 23:01:00 +0800

    Skeleton UI without functionality
    * Change company name from DefaultCompany
    
    * Add prefabs for UI elements

---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class UiController : Singleton<UiController>
+{
+    [SerializeField] private GameObject mainPage;
+    [SerializeField] private GameObject newQuizPage;
+    [SerializeField] private GameObject quizDetailsPage;
+    [SerializeField] private GameObject questionDetailsPage;
+    [SerializeField] private GameObject quizPreviewPage;
+    [SerializeField] private GameObject quizResultsPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
